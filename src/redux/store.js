52f26027e8fe881f4f73dcfe87a62e0442dc39c7
KiraKostina <kostina.kira@gmail.com>
2024-06-02 00:06:59 +0300

    persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import persistReducer from 'redux-persist/es/persistReducer';
+// import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistedContactsReducer = persistReducer(
+  {
+    key: 'items',
+    storage,
+  },
+  contactsReducer
+);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filters: filtersReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
